761d767260dc81b3355c581354c21c65d85f7acb
Sebastian <iwan.sebastian029@gmail.com>
2024-03-18 14:57:08 +0100

    Added requirements.txt

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def hello_world():
     return "Hello World"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
