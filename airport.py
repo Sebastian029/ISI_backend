55416579386da6422689b4f4acd52603a50dc30b
Blazej <jakubczyk.blazej@gmail.com>
2024-05-14 19:22:10 +0200

    pobranie miasta z lotniskiem

--- airport.py
+++ airport.py
@@ -1,5 +1,5 @@
 from config import app, db
-from models import  Airport
+from models import Airport, City
 from flask import request, jsonify
 
 @app.route("/airports", methods=["GET"])
@@ -12,3 +12,24 @@ def get_ports():
         for airport in airports
     ]
     return jsonify(json_airports)
+
+
+@app.route("/city_airports", methods=["GET"])
+def get_city_with_airport():
+    try:
+        results = db.session.query(
+            Airport, City
+        ).join(
+            City, Airport.city_id == City.city_id
+        ).all()
+
+        json_city_airports = [
+            {
+                "airport": city.city_name + " : " + airport.airport_name,
+            }
+            for airport, city in results
+        ]
+        return jsonify(json_city_airports)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
