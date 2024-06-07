93aee2d38ef6a1cfb7496679dad71389c2f794b2
Dominik Jaroszek <dominikjaroszekx@gmail.com>
2024-06-07 21:41:23 +0200

    follow

--- services/flightService.py
+++ services/flightService.py
@@ -8,6 +8,7 @@ from controllers.planeController import *
 from controllers.airlineController import *
 from controllers.ticketController import *
 from controllers.orderController import *
+from controllers.followController import *
 from schemas.flight_schema import *
 from pydantic import ValidationError
 from datetime import datetime
@@ -154,6 +155,71 @@ def get_flights_with_airports():
             "available_seats": flight.available_seats,
             "travel_time": flight.travel_time.strftime('%H:%M:%S'),
             "data_lotu": flight.data_lotu
+        })
+
+    return jsonify(flights_json), 200
+
+
+@app.route('/flights_with_airports_token', methods=['GET'])
+@token_required
+def get_flights_with_airports_token(current_user):
+    try:
+        departure_airport_id = request.args.get('departure_airport_id')
+        arrive_airport_id = request.args.get('arrive_airport_id')
+        data_lotu = request.args.get('data_lotu')
+        
+        if data_lotu:
+            data = FlightSearchSchema(
+                departure_airport_id=departure_airport_id,
+                arrive_airport_id=arrive_airport_id,
+                data_lotu=data_lotu
+            )
+        else:
+            data = FlightSearchSchemaWithoutDate(
+                departure_airport_id=departure_airport_id,
+                arrive_airport_id=arrive_airport_id
+            )
+    except ValidationError as e:
+        return jsonify({"message": e.errors()}), 400
+
+    if data_lotu:
+        try:
+            date = datetime.strptime(data.data_lotu, '%Y-%m-%d')
+        except ValueError:
+            return jsonify({"error": "Invalid date format."}), 400
+
+    airports_dep = get_airports(data.departure_airport_id)
+    airports_arr = get_airports(data.arrive_airport_id)
+
+    if not airports_dep or not airports_arr:
+        return jsonify({'error': 'Departure or arrival airport not found.'}), 404
+
+    dep_airport, dep_city_id, dep_city_name = airports_dep
+    arr_airport, arr_city_id, arr_city_name = airports_arr
+
+    if data_lotu:
+        flights = get_flight_by_data_lotu(dep_airport, arr_airport, data.data_lotu)
+    else:
+        flights = get_flight(dep_airport, arr_airport)
+
+    if not flights:
+        return jsonify({'error': 'No flights found for the given parameters.'}), 404
+
+    flights_json = []
+
+    for flight in flights:
+        is_follow = get_follow(current_user.user_id, flight.flight_id)
+        flights_json.append({
+            "flight_id": flight.flight_id,
+            "departure_airport": dep_airport.airport_name,
+            "departure_city": dep_city_name,
+            "arrival_airport": arr_airport.airport_name,
+            "arrival_city": arr_city_name,
+            "distance": flight.distance,
+            "available_seats": flight.available_seats,
+            "travel_time": flight.travel_time.strftime('%H:%M:%S'),
+            "data_lotu": flight.data_lotu,
+            "is_follow": bool(is_follow)
         })
 
     return jsonify(flights_json), 200
