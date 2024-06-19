dd3436110e4431ad5cfd5a529d66faeecda8d4aa
Dominik Jaroszek <dominikjaroszekx@gmail.com>
2024-06-19 21:51:05 +0200

    Zmiana tokenów

--- app/services/userService.py
+++ app/services/userService.py
@@ -5,6 +5,9 @@ from app.controllers.roleController import all_get_role
 from app.utils import *
 from app.schemas.user_schema import *
 from pydantic import ValidationError
+from app.config import db
+from app.models.token import Token
+from sqlalchemy import update
 
 userbp= blueprints.Blueprint('userbp', __name__)
 
@@ -43,11 +46,14 @@ def login():
 
     user = get_user_by_email(data.email)
     if check_password_controller(user, data.password):
+        
         roles = all_get_role(user)
-        access_token = generate_access_token(user.public_id)
-        refresh_token = generate_refresh_token(user.public_id)
+        access_token = generate_access_token(user.public_id, roles, user.name, user.surname)
+        refresh_token = generate_refresh_token(user.public_id, roles, user.name, user.surname)
+        token = Token(user_id=user.user_id, access_token= access_token, refresh_token=refresh_token)
+        db.session.add(token)
 
-        return jsonify({'access_token': access_token, 'refresh_token': refresh_token, 'roles': roles, 'name': user.name, 'surname':user.surname}), 200
+        return jsonify({'access_token': access_token, 'refresh_token': refresh_token}), 200
     
     return jsonify({'message': 'Nieprawidłowe dane logowania'}), 401
 
@@ -96,16 +102,16 @@ def get_contacts(current_user):
 
 @userbp.route('/logout', methods=['DELETE'])
 def logout():
-    token = None
+    acess_token = None
     if 'x-access-tokens' in request.headers:
-        token = request.headers['x-access-tokens']
+        acess_token = request.headers['x-access-tokens']
         
     refresh_token = None    
     if 'x-refresh-tokens' in request.headers:
         refresh_token = request.headers['x-refresh-tokens']
         
 
-    if revoke_token(refresh_token,token):
+    if revoke_token(acess_token, refresh_token):
         return jsonify({"message": "Refresh and aceess token revoked"}),200
     return jsonify({"message": "Invalid refresh token"}), 400
     
@@ -135,13 +141,16 @@ def refresh():
     if current_user is None:
         return jsonify({'message': 'Invalid refresh token'}), 401
     
-    stored_token = RefreshToken.query.filter_by(token=refresh_token, revoked=False).first()
+    stored_token = Token.query.filter_by(refresh_token=refresh_token).first()
     if not stored_token:
         return jsonify({'message': 'Invalid or expired refresh token'}), 401
-    
-    access_token = generate_access_token(current_user.public_id)
+    roles = all_get_role(current_user)
+    new_access_token = generate_access_token(current_user.public_id, roles, current_user.name, current_user.surname)
 
-    return jsonify({'access_token': access_token})
+    stmt = update(Token).where(Token.token_id == stored_token.token_id).values(acess_token=new_access_token)
+    db.session.execute(stmt)
+    db.session.commit()
+    return jsonify({'access_token': new_access_token})
 
 # @app.route("/users_email", methods=["GET"])
 # def get_contacts_email():
