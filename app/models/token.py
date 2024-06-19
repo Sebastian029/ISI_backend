0b2f3e394dd909cded483e2587767783c5e22b36
Dominik Jaroszek <dominikjaroszekx@gmail.com>
2024-06-19 23:19:14 +0200

    log dziala

--- app/models/token.py
+++ app/models/token.py
@@ -2,14 +2,13 @@ from app.config import db
 
 class Token(db.Model):
     token_id = db.Column(db.Integer, primary_key=True)
-    acess_token = db.Column(db.String(256), nullable=False, unique=True)
+    access_token = db.Column(db.String(256), nullable=False, unique=True)
     refresh_token = db.Column(db.String(256), nullable=False, unique=True)
     user_id = db.Column(db.String(36), db.ForeignKey('user.user_id'), nullable=False)
 
 
 
-    def __init__(self, token, user_id, acess_token, refresh_token):
-        self.token = token
+    def __init__(self, user_id, access_token, refresh_token):
         self.user_id = user_id
-        self.acess_token = acess_token
+        self.acess_token = access_token
         self.refresh_token = refresh_token
